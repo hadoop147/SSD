9badbb83adf600cc2debed198ef1cda102ba6456
Geneci Cruz <gene.fro@hotmail.com>
2020-12-06 17:28:00 +0000

    changed devise users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
           def configure_permitted_parameters
-               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password)}
+               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:fname, :email, :password)}
 
                devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:fname, :lname, :phonenumber, :date_of_birth, :email, :password,
                :current_password)}
