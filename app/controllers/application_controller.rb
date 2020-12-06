a71a5482584e118ed7736c9188d25803d0df47c3
EC2 Default User <ec2-user@ip-172-31-73-3.ec2.internal>
2020-12-06 18:28:29 +0000

    migrate the admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
           def configure_permitted_parameters
                devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:fname, :email, :password)}
 
-               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:fname, :lname, :phonenumber, :date_of_birth, :email, :password,
+               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:fname, :lname, :date_of_birth, :email, :password,
                :current_password)}
           end
 
